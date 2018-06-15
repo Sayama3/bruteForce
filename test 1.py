3305c7fda568e1b2b8dc6a305bba28cdcc54c4ee
Sayama3 <39456336+Sayama3@users.noreply.github.com>
2018-06-16 00:59:11 +0400

    Add files via upload

--- test 1.py
+++ test 1.py
@@ -2,4 +2,13 @@ caractere = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g"
              "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D",
              "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X",
              "Y","Z","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<",
-             "=",">","?","@","[","]","^","_","`","{","|","}","~","\\"]
+             "=",">","?","@","[","]","^","_","`","{","|","}","~","\\"," "]
+
+def motAleatoire(caractere):
+    import random
+    motDePasse = str()
+    nbrDeCaractere = random.randrange(5,20,1)
+    for i in range(nbrDeCaractere):
+        choix = random.randrange(0,94,1)
+        motDePasse += caractere[choix]
+    return motDePasse
